7aa7535cf320225e3b3216ef92573896fa5023d6
H2 <hemoptysisheart@gmail.com>
2022-07-06 00:52:14 +0900

    ✨ Add JPA mapping.

--- src/main/java/com/github/hemoptysisheart/samplejpa/entity/Todo.java
+++ src/main/java/com/github/hemoptysisheart/samplejpa/entity/Todo.java
@@ -1,12 +1,29 @@
 package com.github.hemoptysisheart.samplejpa.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.StringJoiner;
+
 import static java.util.Objects.requireNonNull;
 
+@Entity(name = "Todo")
+@Table(name = "todo",
+    indexes = {@Index(name = "fk_todo_pk_user", columnList = "user ASC"),
+        @Index(name = "fk_todo_pk_status", columnList = "status ASC")})
 public class Todo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
+  @ManyToOne(targetEntity = User.class, optional = false)
+  @JoinColumn(name = "user", nullable = false, updatable = false,
+      foreignKey = @ForeignKey(name = "fk_todo_pk_user"), referencedColumnName = "id")
   private User user;
+  @Column(name = "title", nullable = false)
   private String title;
+  @Column(name = "detail", nullable = false)
   private String detail;
+  @Column(name = "status", nullable = false)
+  @Enumerated
   private Status status;
 
   public Todo() { // JPA only
@@ -57,5 +74,32 @@ public class Todo {
 
   public void setStatus(Status status) {
     this.status = requireNonNull(status);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return this.id == ((Todo) o).id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", Todo.class.getSimpleName() + "[", "]")
+        .add("id=" + this.id)
+        .add("user=" + this.user)
+        .add("title='" + this.title + "'")
+        .add("detail='" + this.detail + "'")
+        .add("status=" + this.status)
+        .toString();
   }
 }
--- src/main/java/com/github/hemoptysisheart/samplejpa/entity/User.java
+++ src/main/java/com/github/hemoptysisheart/samplejpa/entity/User.java
@@ -1,10 +1,22 @@
 package com.github.hemoptysisheart.samplejpa.entity;
 
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.StringJoiner;
+
 import static java.util.Objects.requireNonNull;
 
+@Entity(name = "User")
+@Table(name = "user",
+    uniqueConstraints = {@UniqueConstraint(name = "uq_user_name", columnNames = {"name"})})
 public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, insertable = false, updatable = false)
   private long id;
+  @Column(name = "name", nullable = false, updatable = false, unique = true)
   private String name;
+  @Column(name = "bio")
   private String bio;
 
   public User() { // JPA only
@@ -33,5 +45,30 @@ public class User {
 
   public void setBio(String bio) {
     this.bio = requireNonNull(bio);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    return this.id == ((User) o).id;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.id);
+  }
+
+  @Override
+  public String toString() {
+    return new StringJoiner(", ", User.class.getSimpleName() + "[", "]")
+        .add("id=" + this.id)
+        .add("name='" + this.name + "'")
+        .add("bio='" + this.bio + "'")
+        .toString();
   }
 }
